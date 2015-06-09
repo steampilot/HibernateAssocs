30d539ba6c73afebd7940cf9ec73f5f6a5fb87a4
steampilot <github@steampilot.ch>
2015-06-09 09:42:58 +0200

    changed database to create

---
+++ src/data/Dao.java
@@ -0,0 +1,12 @@
+package data;
+
+/**
+ * Created by steampilot on 09.06.15.
+ */
+public interface Dao  {
+    public void create();
+    public void read();
+    public void update();
+    public void delete();
+
+}
---
+++ src/data/DaoImpl.java
@@ -0,0 +1,8 @@
+package data;
+
+/**
+ * Created by steampilot on 09.06.15.
+ */
+public abstract class DaoImpl  implements Dao{
+
+}
